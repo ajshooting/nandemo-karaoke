18927942db5068b500f289c00360d5a25546e021
ajshooting <ajshooting.aj@gmail.com>
2024-12-27 23:00:22 +0900

    理解コメント/歌詞表示を調整(サイズ・次の歌詞)

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -14,10 +14,10 @@ from PyQt6.QtCore import pyqtSlot, QTimer, Qt, QThread, pyqtSignal
 from PyQt6.uic import loadUi
 from PyQt6.QtGui import QPixmap, QDragEnterEvent, QDropEvent
 
-# widgetsフォルダからカスタムウィジェットをインポート
+# カスタムウィジェットをインポート
 from src.gui.widgets.pitch_bar import PitchBar
 
-# 他の処理系モジュールをインポート (必要に応じて)
+# モジュールをインポート
 from src.audio.player import Player
 from src.audio.recorder import Recorder
 from src.audio.separator import Separator
@@ -49,20 +49,18 @@ class RecognitionThread(QThread):
 
 
 class SeparationThread(QThread):
-    finished_signal = pyqtSignal(dict)  # 分離されたファイルのパスを辞書で送信
+    finished_signal = pyqtSignal(dict)
     error_signal = pyqtSignal(str)
 
     def __init__(self, input_path):
         super().__init__()
         self.input_path = input_path
-        self.separator = Separator()  # Separatorクラスのインスタンスを作成
+        self.separator = Separator()
 
     def run(self):
         try:
-            separated_paths = self.separator.separate(
-                self.input_path
-            )  # separateメソッドを呼び出す
-            self.finished_signal.emit(separated_paths)  # 分離されたパスを送信
+            separated_paths = self.separator.separate(self.input_path)
+            self.finished_signal.emit(separated_paths)
         except Exception as e:
             self.error_signal.emit(str(e))
 
@@ -70,15 +68,11 @@ class SeparationThread(QThread):
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        loadUi(
-            "src/gui/ui/main_window.ui", self
-        )  # Qt Designerで作成したUIファイルを読み込む
+        loadUi("src/gui/ui/main_window.ui", self)
 
-        # ウィジェットの取得 (UIファイルで設定したobjectNameでアクセス)
+        # ウィジェットの取得 (.uiのobjectName)
         self.lyrics_label = self.findChild(QLabel, "lyricsLabel")
-        self.pitch_bar_widget = self.findChild(
-            PitchBar, "pitchBarWidget"
-        )  # UIファイルで設定したPitchBarウィジェット
+        self.pitch_bar_widget = self.findChild(PitchBar, "pitchBarWidget")
         self.play_button = self.findChild(QPushButton, "playButton")
         self.stop_button = self.findChild(QPushButton, "stopButton")
         self.score_label = self.findChild(QLabel, "scoreLabel")
@@ -88,9 +82,8 @@ class MainWindow(QMainWindow):
         self.drop_area.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.drop_area.setFrameStyle(QLabel.Shape.StyledPanel | QLabel.Shadow.Sunken)
         self.drop_area.setAcceptDrops(True)
-        self.findChild(QVBoxLayout, "verticalLayout").insertWidget(
-            0, self.drop_area
-        )  # verticalLayoutの先頭に挿入
+        # verticalLayoutの先頭に挿入
+        self.findChild(QVBoxLayout, "verticalLayout").insertWidget(0, self.drop_area)
 
         # ボタンのシグナルとスロットを接続
         self.play_button.clicked.connect(self.on_play_clicked)
@@ -107,19 +100,18 @@ class MainWindow(QMainWindow):
         self.timer.timeout.connect(self.update_lyrics_display)  # 歌詞更新処理を追加
         self.timer.setInterval(100)  # 例: 100msごとに更新
 
-        # 他の処理系モジュールの初期化 (必要に応じて)
+        # 他の処理系モジュールの初期化
         self.audio_player = Player()
         self.audio_recorder = Recorder()
         self.pitch_extractor = PitchExtractor()
         self.lyric_synchronizer = Synchronizer()
-        self.recognizer = Recognizer()  # Recognizerのインスタンス化
+        self.recognizer = Recognizer()
         self.recognition_thread = None
 
         # 初期設定
         self.current_song_path = ""  # 現在の曲のパス
-        self.separated_song_paths = (
-            {}
-        )  # 分離後の曲のパスを格納する辞書 (例: {'vocals': '...', 'accompaniment': '...' })
+        # 分離後の曲のパスを格納する辞書 (例: {'vocals': '...', 'accompaniment': '...' })
+        self.separated_song_paths = {}
         self.lyrics = []  # 歌詞リスト
         self.pitch_data = []  # 音程データ
         self.accompaniment_path = None  # 分離後の伴奏ファイルのパスを保存する変数
@@ -130,9 +122,7 @@ class MainWindow(QMainWindow):
         self.current_word_index = 0  # 現在色付け中の単語のインデックス
 
         self.lyrics_label = self.findChild(QLabel, "lyricsLabel")
-        self.lyrics_label.setTextFormat(
-            Qt.TextFormat.RichText
-        )  # リッチテキストを有効にする
+        self.lyrics_label.setTextFormat(Qt.TextFormat.RichText)  # リッチテキストを有効
 
     def dragEnterEvent(self, event: QDragEnterEvent):
         if event.mimeData().hasUrls():
@@ -226,14 +216,11 @@ class MainWindow(QMainWindow):
             return
 
         if self.accompaniment_path:
-            print(
-                f"再生ボタンがクリックされました。再生ファイル: {self.accompaniment_path}"
-            )
+            print(f"再生: {self.accompaniment_path}")
             self.audio_player.play(self.accompaniment_path)
-        elif self.current_song_path:  # 分離前のオリジナル音源を再生
-            print(
-                f"再生ボタンがクリックされました。再生ファイル: {self.current_song_path}"
-            )
+        elif self.current_song_path:
+            # 分離前のオリジナル音源を再生
+            print(f"再生: {self.current_song_path}")
             self.audio_player.play(self.current_song_path)
         else:
             QMessageBox.warning(
@@ -242,6 +229,8 @@ class MainWindow(QMainWindow):
                 "音源ファイルが選択されていません。先に音源ファイルをドロップまたは選択してください。",
             )
             return
+
+        self.lyrics_label.setText("")
 
         self.timer.start()
         self.current_lyric_index = 0  # 再生開始時にインデックスをリセット
@@ -268,6 +257,7 @@ class MainWindow(QMainWindow):
         #     self.pitch_bar_widget.set_pitch(pitch_value)
         pass
 
+    # ! これ使われてなくない？
     def set_lyrics(self, lyrics):
         self.lyrics = lyrics
         # 歌詞をQLabelに表示する処理 (例: 最初の歌詞を表示)
@@ -285,9 +275,14 @@ class MainWindow(QMainWindow):
 
         if self.current_segment_index < len(self.recognized_lyrics):
             segment = self.recognized_lyrics[self.current_segment_index]
+            next_segment = (
+                self.recognized_lyrics[self.current_segment_index + 1]
+                if self.current_segment_index + 1 < len(self.recognized_lyrics)
+                else None
+            )
             if segment["start"] <= current_time < segment["end"]:
                 # 現在のフレーズを表示
-                self.display_karaoke_text(segment)
+                self.display_karaoke_text(segment, next_segment)
             elif current_time >= segment["end"]:
                 # 現在のフレーズの終了時間を過ぎたら、次のフレーズへ
                 self.current_segment_index += 1
@@ -295,10 +290,11 @@ class MainWindow(QMainWindow):
                 # 次のフレーズが存在すれば、すぐに表示
                 if self.current_segment_index < len(self.recognized_lyrics):
                     self.display_karaoke_text(
-                        self.recognized_lyrics[self.current_segment_index]
+                        self.recognized_lyrics[self.current_segment_index], next_segment
                     )
             else:
                 # まだフレーズの開始時間になっていない
+                self.display_karaoke_text(segment, next_segment)
                 pass
         elif (
             self.recognized_lyrics and current_time >= self.recognized_lyrics[-1]["end"]
@@ -307,7 +303,7 @@ class MainWindow(QMainWindow):
             self.lyrics_label.setText("")
             self.timer.stop()
 
-    def display_karaoke_text(self, segment):
+    def display_karaoke_text(self, segment, next_segment):
         karaoke_text = ""
         current_time = self.audio_player.get_current_time()
 
@@ -320,7 +316,7 @@ class MainWindow(QMainWindow):
                     else 0
                 )
                 highlight_length = int(len(word_info["word"]) * ratio)
-                highlighted_word = f"<span style='color: red;'>{word_info['word'][:highlight_length]}</span>{word_info['word'][highlight_length:]}"
+                highlighted_word = f"<span style='color: red; font-size: 20px;'>{word_info['word'][:highlight_length]}</span>{word_info['word'][highlight_length:]}"
                 karaoke_text += " " + highlighted_word
             elif current_time >= word_info["end"]:
                 karaoke_text += (
@@ -329,8 +325,16 @@ class MainWindow(QMainWindow):
             else:
                 karaoke_text += " " + word_info["word"]
 
+        # TODO いい感じに調整
+        karaoke_text += (
+            "<br><br><span style='font-size: 10px;'>" + next_segment["text"] + "</span>"
+            if next_segment
+            else ""
+        )
+
         self.lyrics_label.setText(karaoke_text)
 
+    # TODO: さいてん、する、、？
     def update_score(self, score):
         self.score_label.setText(f"スコア: {score}")
 
